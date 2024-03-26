0b1433029ae69a2534184a22338e26f284292ac3
William Boyard <williamboyard1@gmail.com>
2024-03-26 09:19:58 +0100

    TDD CSVMake

--- CreditImmo/CreditImmo/Program.cs
+++ CreditImmo/CreditImmo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace CreditImmo 
 {
@@ -6,7 +7,7 @@ namespace CreditImmo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
